10b6e863939733d27488d680a882c89cc3084d41
Patrick Hess <phess.ph@gmail.com>
2023-03-14 10:43:58 -0700

    update channel id

--- heathbot/lambda/heathbot.py
+++ heathbot/lambda/heathbot.py
@@ -60,7 +60,7 @@ async def post_to_discord(token, url_to_post):
     client = discord.Client(intents=intents)
     await client.login(token=token)
     await client.wait_until_ready()
-    channel = client.get_channel(12324234183172)
+    channel = client.get_channel(1083473604185960548)
     async with aiohttp.ClientSession() as session:
         async with session.get(url_to_post) as resp:
             if resp.status != 200:
